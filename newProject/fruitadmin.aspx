cd13381c0bf8f6b74d4cf449dd83ccde7eae529f
Vatsal Kansara <kansaravatsal17@gmail.com>
2024-11-01 18:09:06 +0530

    Registration Form Created

--- newProject/fruitadmin.aspx
+++ newProject/fruitadmin.aspx
@@ -3,8 +3,5 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
    <h1 style="padding-top:70px;margin-left:50%"> this is fruit admin</h1>
-    <p style="padding-top:70px;margin-left:50%"> 
-        <asp:GridView ID="GridView1" runat="server">
-        </asp:GridView>
-    </p>
+  
 </asp:Content>
--- newProject/fruitadmin.aspx.designer.cs
+++ newProject/fruitadmin.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace newProject
 
     public partial class fruitadmin
     {
-
-        /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
